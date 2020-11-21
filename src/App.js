ba97d775cb983e5a1fb77e81de1a4f69eaaf21ee
Parsa <akbariparsa1209@gmail.com>
2020-11-21 20:31:38 +0330

    Completed App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Chat from './Chat.js'
+import Login from './Login.js'
+import Sidebar from './Sidebar.js'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { useStateValue } from './StateProvider.js'
 
 function App() {
+  const [{ user }, dispatch] = useStateValue()
+
   return (
+    // BEM naming convention.
     <div className="App">
+      { !user ? (
+          <Login />
+      ): (
+        <div className="app__body">
+          <BrowserRouter>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomID">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </div>
+      )}
     </div>
   );
 }
