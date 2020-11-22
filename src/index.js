3667c3ab6ce47dd80fbc04e41eae0ba3e8d5f2a3
Parsa <akbariparsa1209@gmail.com>
2020-11-22 10:40:21 +0330

    Fixed imports (moving issue.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StateProvider } from './StateProvider.js';
-import reducer, { initialState } from './reducer';
+import { StateProvider } from './context/StateProvider.js';
+import reducer, { initialState } from './context/reducer';
+import './styles/index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
